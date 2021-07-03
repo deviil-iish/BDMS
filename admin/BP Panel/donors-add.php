6ddfd783aeaa4cb58b03189433235a18a01eb14a
valkyrieii <71064880+valkyrieii@users.noreply.github.com>
2021-07-03 23:23:24 +0500

    Add files via upload

---
+++ admin/BP Panel/donors-add.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="" sizes="16x16" href="../../images/logo.PNG">
+    <title>Blood+ Admin</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../plugins/bower_components/bootstrap-extension/css/bootstrap-extension.css" rel="stylesheet">
+    <!-- Menu CSS -->
+    <link href="../plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.css" rel="stylesheet">
+    <!-- morris CSS -->
+    <link href="../plugins/bower_components/morrisjs/morris.css" rel="stylesheet">
+    <!-- animation CSS -->
+    <link href="css/animate.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/style.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/b20d4e7598.js" crossorigin="anonymous"></script>
+
+    <!-- color CSS -->
+    <link href="css/colors/megna.css" id="theme" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div class="preloader">
+        <div class="cssload-speeding-wheel"></div>
+    </div>
+    <div id="wrapper">
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top m-b-0">
+            <div class="navbar-header"> <a class="navbar-toggle hidden-sm hidden-md hidden-lg " href="javascript:void(0)" data-toggle="collapse" data-target=".navbar-collapse"><i class="ti-menu"></i></a>
+                <div class="top-left-part"><a class="logo" href="aindex.html"><b><img src="../../images/hlogo.png" alt="home" /></b><span class="hidden-xs"><strong>Blood</strong>+</span></a></div>
+                <ul class="nav navbar-top-links navbar-left hidden-xs">
+                    <li><a href="javascript:void(0)" class="open-close hidden-xs waves-effect waves-light"><i class="icon-arrow-left-circle ti-menu"></i></a></li>
+                    <li>
+                        <form role="search" class="app-search hidden-xs">
+                            <input type="text" placeholder="Search..." class="form-control"> <a href=""><i class="fa fa-search"></i></a> </form>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-header -->
+            <!-- /.navbar-top-links -->
+            <!-- /.navbar-static-side -->
+        </nav>
+        <!-- Left navbar-header -->
+        <div class="navbar-default sidebar" role="navigation">
+            <div class="sidebar-nav navbar-collapse slimscrollsidebar">
+                <ul class="nav" id="side-menu">
+                    <li class="sidebar-search hidden-sm hidden-md hidden-lg">
+                        <!-- input-group -->
+                        <div class="input-group custom-search-form">
+                            <input type="text" class="form-control" placeholder="Search..."> <span class="input-group-btn">
+            <button class="btn btn-default" type="button"> <i class="fa fa-search"></i> </button>
+            </span> </div>
+                        <!-- /input-group -->
+                    </li>
+
+                    <li class="nav-small-cap m-t-10">--- Main Menu</li>
+                    <li> <a href="aindex.html" class="waves-effect"><i class="ti-dashboard p-r-10"></i> <span class="hide-menu">Dashboard</span></a> </li>
+                    <li class="nav-small-cap m-t-10">--- Professional</li>
+                    <li> <a href="javascript:void(0);" class="waves-effect"><i class="fa fa-user-md p-r-10"></i> <span class="hide-menu"> Donors <span class="fa arrow"></span></span></a>
+                        <ul class="nav nav-second-level">
+                            <li> <a href="donors.php">All Donors</a> </li>
+                            <li> <a href="donors-add.php">Add Donors</a> </li>
+                            <li> <a href="donors-edit.php">Edit Donors</a> </li>
+                        </ul>
+                    </li>
+                    <li> <a href="javascript:void(0);" class="waves-effect"><i class="icon-people p-r-10"></i> <span class="hide-menu"> Recipients <span class="fa arrow"></span></span></a>
+                        <ul class="nav nav-second-level">
+                            <li> <a href="recipient.php">All Recipients</a> </li>
+                            <li> <a href="recipient-add.php">Add Recipients</a> </li>
+                            <li> <a href="recipient-edit.php">Edit Recipients</a> </li>
+                        </ul>
+                    </li>
+                    <li> <a href="javascript:void(0);" class="waves-effect"><i class="fas fa-hospital-alt"></i> <span class="hide-menu"> Hospitals <span class="fa arrow"></span></span></a>
+                        <ul class="nav nav-second-level">
+                            <li> <a href="hospital.php">All Hospitals</a> </li>
+                            <li> <a href="hospital-add.php">Add Hospitals</a> </li>
+                            <li> <a href="hospital-edit.php">Edit Hospitals</a> </li>
+                        </ul>
+                    </li>
+                    <li> <a href="javascript:void(0);" class="waves-effect"><i class="fas fa-hospital-alt"></i> <span class="hide-menu"> Blood Banks <span class="fa arrow"></span></span></a>
+                        <ul class="nav nav-second-level">
+                            <li> <a href="bloodbank.php">All Blood Banks</a> </li>
+                            <li> <a href="bloodbank-add.php">Add Blood Banks</a> </li>
+                            <li> <a href="bloodbank-edit.php">Edit Blood Banks</a> </li>
+                        </ul>
+                    </li>
+                    <li> <a href="javascript:void(0);" class="waves-effect"><i class="fas fa-hospital-user"></i> <span class="hide-menu"> Staff <span class="fa arrow"></span></span></a>
+                        <ul class="nav nav-second-level">
+                            <li> <a href="staff.php">All Staff</a> </li>
+                            <li> <a href="staff-add.php">Add Staff</a> </li>
+                            <li> <a href="staff-edit.php">Edit Staff</a> </li>
+                        </ul>
+                    </li>
+                    <li> <a href="javascript:void(0);" class="waves-effect"><i class="fas fa-syringe"></i> <span class="hide-menu"> Blood Stock <span class="fa arrow"></span></span></a>
+                        <ul class="nav nav-second-level">
+                            <li> <a href="bloodstock.php">All Blood Stock</a> </li>
+                            <li> <a href="bloodstock-add.php">Add Blood Stock</a> </li>
+                            <li> <a href="bloodstock-edit.php">Edit Blood Stock</a> </li>
+                        </ul>
+                    </li>
+                    <li><a href="../../index.html" class="waves-effect"><i class="icon-logout fa-fw"></i> <span class="hide-menu">Log out</span></a></li>
+
+                </ul>
+            </div>
+        </div>
+        <!-- Left navbar-header end -->
+        <!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row bg-title">
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                        <h4 class="page-title">Blood+ Dashboard</h4>
+                    </div>
+                    <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+                        <ol class="breadcrumb">
+                            <li><a href="aindex.html">Admin</a></li>
+                            <li class="active">Add Donors</li>
+                        </ol>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="white-box">
+                            <h3 class="box-title">Basic Information</h3>
+                            <form class="form-material form-horizontal">
+                                <div class="form-group">
+                                    <label class="col-md-12">Name</span>
+                                    </label>
+                                    <div class="col-md-12">
+                                        <input type="text" name="dname" class="form-control" placeholder="Name"> </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-12" for="city">City</span>
+                                    </label>
+                                    <div class="col-md-12">
+                                        <input type="text" name="city" class="form-control" placeholder="City of Residence"> </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-12" for="location">Address</span>
+                                    </label>
+                                    <div class="col-md-12">
+                                        <input type="text" name="location" class="form-control" placeholder="Address"> </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-12" for="phone">Contact No.</span>
+                                    </label>
+                                    <div class="col-md-12">
+                                        <input type="text" name="phone" class="form-control" placeholder="Contact Phone Number"> </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-12" for="cnic">CNIC</span>
+                                    </label>
+                                    <div class="col-md-12">
+                                        <input type="text" name="cnic" class="form-control" placeholder="CNIC"> </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-12">Gender</label>
+                                    <div class="col-sm-12">
+                                        <select name="gender" class="form-control" placeholder="Select Gender">
+                                            <option>Male</option>
+                                            <option>Female</option>
+                                            <option>Other</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-12">Blood Group</label>
+                                    <div class="col-sm-12">
+                                        <select name="bloodgroup" class="form-control" placeholder="Select Blood Group">
+                                            <option>A+</option>
+                                            <option>A-</option>
+                                            <option>B+</option>
+                                            <option>B-</option>
+                                            <option>AB+</option>
+                                            <option>AB-</option>
+                                            <option>O+</option>
+                                            <option>O-</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-12" for="dob">Date of Birth</span>
+                                    </label>
+                                    <div class="col-md-12">
+                                        <input type="text" name="dob" class="form-control mydatepicker" placeholder="Date of Birth"> </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-12" for="ldd">Last Donation Date</span>
+                                    </label>
+                                    <div class="col-md-12">
+                                        <input type="text" name="ldd" class="form-control mydatepicker" placeholder="Last Donation Date"> </div>
+                                </div>
+                                <button type="submit" class="btn btn-info waves-effect waves-light m-r-10">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /#page-wrapper -->
+    </div>
+    <!-- /#wrapper -->
+    <!-- jQuery -->
+    <script src="../plugins/bower_components/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="bootstrap/dist/js/tether.min.js"></script>
+    <script src="bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="../plugins/bower_components/bootstrap-extension/js/bootstrap-extension.min.js"></script>
+    <!-- Menu Plugin JavaScript -->
+    <script src="../plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js"></script>
+    <!--slimscroll JavaScript -->
+    <script src="js/jquery.slimscroll.js"></script>
+    <!--Wave Effects -->
+    <script src="js/waves.js"></script>
+    <!--Morris JavaScript -->
+    <script src="../plugins/bower_components/raphael/raphael-min.js"></script>
+    <script src="../plugins/bower_components/morrisjs/morris.js"></script>
+    <!-- Sparkline chart JavaScript -->
+    <script src="../plugins/bower_components/jquery-sparkline/jquery.sparkline.min.js"></script>
+    <!-- jQuery peity -->
+    <script src="../plugins/bower_components/peity/jquery.peity.min.js"></script>
+    <script src="../plugins/bower_components/peity/jquery.peity.init.js"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="js/custom.min.js"></script>
+    <script src="js/dashboard1.js"></script>
+    <!--Style Switcher -->
+    <script src="../plugins/bower_components/styleswitcher/jQuery.style.switcher.js"></script>
+</body>
+
+</html>
